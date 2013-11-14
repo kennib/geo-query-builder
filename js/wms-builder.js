025775c45f5581ec48a11c31491ad6a7a2021430
Kenni Bawden <kenni.bawden@nicta.com.au>
2013-11-14 17:41:40 +1100

    Added service type selection for the url builder.

--- js/wms-builder.js
+++ js/wms-builder.js
@@ -3,10 +3,25 @@ var wmsBuilder = angular.module("wms-builder", [
 ]);
 
 wmsBuilder.value("hosts", {
-  "NICTA - GeoTopo250K": "http://geospace.research.nicta.com.au:8080/geotopo_250k/web/",
-  "NICTA - Admin Bounds": "http://geospace.research.nicta.com.au:8080/admin_bnds/web/",
+  "NICTA - GeoTopo250K": "http://geospace.research.nicta.com.au:8080/geotopo_250k/web/ows",
+  "NICTA - Admin Bounds": "http://geospace.research.nicta.com.au:8080/admin_bnds/web/ows",
 });
 
-wmsBuilder.controller("builder", ["$scope", "hosts", function($scope, hosts) {
-  $scope.hosts = hosts;
-}]);
+wmsBuilder.value("serviceTypes", {
+  "WMS": "WMS",
+  "WFS": "WFS",
+});
+
+wmsBuilder.controller("builder", ["$scope",
+  "hosts", "serviceTypes",
+  function($scope, hosts, serviceTypes) {
+    $scope.hosts = hosts;
+    $scope.host = hosts["NICTA - Admin Bounds"];
+
+    $scope.serviceTypes = serviceTypes;
+    $scope.serviceType = serviceTypes["WMS"];
+
+    $scope.url = function() {
+      return $scope.host + '?' + 'service=' + $scope.serviceType;
+    }
+  }]);
