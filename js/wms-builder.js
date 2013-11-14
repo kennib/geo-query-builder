6d8ea657a826cc71579df5d381a24e38d2bc141b
Kenni Bawden <kenni.bawden@nicta.com.au>
2013-11-14 17:25:30 +1100

    Added the host selection piece of the builder.

--- js/wms-builder.js
+++ js/wms-builder.js
@@ -2,7 +2,11 @@ var wmsBuilder = angular.module("wms-builder", [
   "ngRoute",
 ]);
 
+wmsBuilder.value("hosts", {
+  "NICTA - GeoTopo250K": "http://geospace.research.nicta.com.au:8080/geotopo_250k/web/",
+  "NICTA - Admin Bounds": "http://geospace.research.nicta.com.au:8080/admin_bnds/web/",
+});
 
-wmsBuilder.controller("builder", ["$scope", function($scope) {
-  $scope.url = "http://geospace.research.nicta.com.au:8080/admin_bnds/web/";
+wmsBuilder.controller("builder", ["$scope", "hosts", function($scope, hosts) {
+  $scope.hosts = hosts;
 }]);
